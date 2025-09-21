58d206d12e4c7e1f5e9773602aebcddb86a5487e
karim rgaibi <karimrgaibi@gmail.com>
2025-09-21 20:23:09 +0200

    Creating JPA repository

---
+++ src/main/java/com/springbootinitializr/API_data/topics/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.springbootinitializr.API_data.topics;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	
+	
+
+}
